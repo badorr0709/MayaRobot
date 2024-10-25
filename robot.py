b6db4184fa6936b8bf5513decbdef9a405a49a82
badorr0709 <badorr@wpi.edu>
2024-10-25 09:30:03 -0400

    update the inital veriables for the objects within eachList

--- robot.py
+++ robot.py
@@ -2,27 +2,24 @@ import maya.cmds as cmds
     class Robot:
         def __init__(self):
             head = [cmds.sphere(pivot=[0.0, 10.0, 0.0], r=2, name="centerOfHead"),
-                    cmds.polyCube(sx=0.5, sy=4.0, sz=1.0, axis=[12.0, 10.0, 0.0], name="leftEar"),
+                    cmds.polyCube(sx=0.5, sy=2.0, sz=1.0, axis=[12.0, 10.0, 0.0], name="leftEar"),
                     cmds.polyCube(sx=0.5, sy=4.0, sz=1.0, axis=[-12.0, 10, 0.0], name="rightEar"),
-                    cmds.polyCube(sx=0.5, sy=0.5, sz=0.5,name = "neck")]
-            body = [cmds.polyCube(sx=9, sy=4.0, sz=3.0, axis=[0.0, 6.0, 0], name="upperChest"),
-                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="midChest"),
-                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="lowerChest")]
-            leg1 = [cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="rightThigh"),
-                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="rightCalf"),
-                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="rightFoot")]
-            leg2 = [cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="leftThigh"),
-                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="leftCalf"),
-                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="leftFoot")]
-            arm1 = [cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], nam="leftBicep"),
-                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="leftForarm"),
-                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="hand")]
-            arm2 = [cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="rightBicep"),
-                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="rightForarm"),
-                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[0.0, 6.0, 0], name="hand")]
-
-                self.initHeadShape()
-                self.initBodyShape()
+                    cmds.polyCube(sx=0.5, sy=0.5, sz=0.5,axis=[0, 8, 0.0],name = "neck")]
+            body = [cmds.polyCube(sx=9.0, sy=4.0, sz=3.0, axis=[0.0, 6.0, 0.0], name="upperChest"),
+                    cmds.polyCube(sx=6.0, sy=2.0, sz=2.6, axis=[0.0,4.5 .0, 0.0], name="midChest"),
+                    cmds.polyCube(sx=6.0, sy=2.0, sz=2.5, axis=[0.0, 3.0, 0.0], name="lowerChest")]
+            leg1 = [cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[1.0, 3.0, 0.0], name="rightThigh"),
+                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[1.0, 2.0, 0.0], name="rightCalf"),
+                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[1.0, 0.0, 0.0], name="rightFoot")]
+            leg2 = [cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[-1.0, 3.0, 0.0], name="rightThigh"),
+                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[-1.0, 2.0, 0.0], name="rightCalf"),
+                    cmds.polyCube(sx=.5, sy=4.0, sz=1.0, axis=[-1.0, 0.0, 0.0], name="rightFoot")]
+            arm1 = [cmds.polyCube(sx=1.0, sy=2.0, sz=1.0, axis=[4.5, 6.0, 0.0], nam="leftBicep"),
+                    cmds.polyCube(sx=0.5, sy=2.0, sz=1.0, axis=[4.5, 4.0, 0.0], name="leftForarm"),
+                    cmds.polyCube(sx=0.5, sy=.5, sz=0.5, axis=[4.5, 2.0, 0.0], name="hand")]
+            arm2= [cmds.polyCube(sx=1.0, sy=2.0, sz=1.0, axis=[-4.5, 6.0, 0.0], nam="leftBicep"),
+                    cmds.polyCube(sx=0.5, sy=2.0, sz=1.0, axis=[-4.5, 4.0, 0.0], name="leftForarm"),
+                    cmds.polyCube(sx=0.5, sy=.5, sz=0.5, axis=[-4.5, 2.0, 0.0], name="hand")]
 
 
 

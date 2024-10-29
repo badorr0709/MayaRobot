b24ff75e82a0c110631cf435aa0ed92fb8b60484
Benjamin Dorr <59935137+badorr0709@users.noreply.github.com>
2024-10-29 07:58:57 -0400

    working on my robot model in maya

--- robot.py
+++ robot.py
@@ -21,21 +21,36 @@ class Robot:
             cmds.polyCube(name="leftForarm"),
             cmds.polyCube(name="hand")]
         self.headsetup()
+        self.bodysetup()
+        self.legsetup()
+        self.armsetup()
+
     def headsetup(self):
-        for x in range(len(self.head)-1):
-            match x:
-                case 0:
-                    cmds.scale(1,1,1,self.head[x])
-                    break
-                case 1:
-                    cmds.scale(.5,3,1,self.head[x])
-                    break
-                case 2:
-                    cmds.scale(.5,3,1,self.head[x])
-                    break
-                case 3:
-                    cmds.scale(.5,.5,.5,self.head[x])
-                    break
+        cmds.scale(1,1,1,self.head[0])
+        cmds.move(0,10,0,self.head[0])
+        cmds.scale(.5,5,1,self.head[1])
+        cmds.rotate(43.0,0,0,self.head[1])
+        cmds.move(2,10,0,self.head[1])
+        cmds.scale(.5,5,1,self.head[2])
+        cmds.move(-2,10,0,self.head[2])
+        cmds.rotate(43.0, 0, 0, self.head[2])
+        cmds.scale(1,2,1,self.head[3])
+        cmds.move(0,8,0,self.head[3])
+
+    def bodysetup(self):
+        cmds.scale(5,3,3,self.body[0])
+        cmds.move(0,6.1,0,self.body[0])
+        cmds.scale(3.5, 2, 3, self.body[1])
+        cmds.move(0,3.756,0,self.body[1])
+        cmds.scale(2, 1, 2, self.body[2])
+        cmds.move(0,2.463,0,self.body[2])
+    def legsetup(self):
+        cmds.scale(1,3,3,self.legLeft[0])
+        cmds.scale(1, 3, 1, self.legLeft[0])
+        cmds.scale(1, 3, 1, self.legLeft[0])
+
+    def armsetup(self):
+
 
 
 
